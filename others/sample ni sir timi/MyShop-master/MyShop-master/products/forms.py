26e6ba8f35c1ff8bcc39b352c0e084dac24dd619
Nichol <nicholjohnfamadico@gmail.com>
2021-04-28 20:51:00 +0800

    Sample ni Sir Timi

---
+++ others/sample ni sir timi/MyShop-master/MyShop-master/products/forms.py
@@ -0,0 +1,44 @@
+from django.forms import ModelForm, TextInput, PasswordInput, CharField, HiddenInput, NumberInput
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth.models import User 
+from products.models import * 
+
+class UserForm(UserCreationForm):
+    attrs = { 'class': 'form-control', 'id':'floatingInput', 'placeholder':'Enter Password' , 'required': True, } 
+    password1 =  CharField( widget=PasswordInput(attrs=attrs) )
+    password2 =  CharField( widget=PasswordInput(attrs=attrs) )
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        widgets = { 
+            'first_name': TextInput( attrs={ 'class': 'form-control', 'id':'floatingInput', 'placeholder':'Enter First Name', 'required': True, } ),
+            'last_name': TextInput( attrs={ 'class': 'form-control', 'id':'floatingInput', 'placeholder':'Enter Last Name', 'required': True, } ),
+            'username': TextInput( attrs={ 'class': 'form-control', 'id':'floatingInput' , 'placeholder':'Enter Username', 'required': True, } ),
+            'email': TextInput( attrs={ 'type':'email', 'class': 'form-control', 'id':'floatingInput', 'placeholder':'Enter Email Address', 'required': True, } ),
+        }
+
+class ShoppingCartForm(ModelForm):
+    class Meta:
+        model = ShoppingCart
+        fields = "__all__"
+        widgets = {
+            'user':  HiddenInput( attrs = {'type':'hidden'} ),
+            'book':  HiddenInput( attrs = {'type':'hidden'} ),
+            'quantity': NumberInput ( attrs = {'class':'form-control', 'min':'1'} ),
+        }
+
+'''
+class UserForm(ModelForm):
+    class Meta: 
+        model = User
+        fields = "__all__"
+        widgets = { 
+            'first_name': TextInput( attrs={ 'class': 'form-control', 'id':'floatingInput', 'placeholder':'name@example.com' , 'required': True, } ),
+            'last_name': TextInput( attrs={ 'class': 'form-control', 'id':'floatingInput', 'placeholder':'name@example.com' , 'required': True, } ),
+            'username': TextInput( attrs={ 'class': 'form-control', 'id':'floatingInput', 'placeholder':'name@example.com' , 'required': True, } ),
+            'email': TextInput( attrs={ 'type':'email', 'class': 'form-control', 'id':'floatingInput', 'placeholder':'name@example.com' , 'required': True, } ),
+            'password': PasswordInput( attrs={ 'class': 'form-control', 'id':'floatingInput', 'placeholder':'name@example.com' , 'required': True, } )
+        }
+'''
---
+++ others/sample ni sir timi/MyShop-master/MyShop-master/products/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100, null=True)
+    author = models.CharField(max_length=100, null=True)
+    price = models.FloatField()
+    description = models.CharField(max_length=500, null=True)
+    weight = models.FloatField(null=True)
+    dimensions = models.CharField(max_length=50, null=True)
+    edition = models.CharField(max_length=50, null=True)
+    language = models.CharField(max_length=50, null=True)
+    pages = models.IntegerField(null=True)
+    subtitle = models.CharField(max_length=500, null=True)
+    is_available = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=100, null=True)
+    rating = models.IntegerField(
+        null=True,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(0)
+        ]
+    )
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.IntegerField(
+        null=True,
+        validators=[
+            MinValueValidator(1)
+        ]
+    )
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+'''
+class User(models.Model):
+    first_name = models.CharField(max_length=100, null=True)
+    last_name = models.CharField(max_length=100, null=True)
+    username = models.CharField(max_length=100, null=True) 
+    email = models.CharField(max_length=100, null=True)
+    password = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.first_name+" "+self.last_name
+'''
