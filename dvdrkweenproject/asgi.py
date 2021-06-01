c6fba1aeaddc6a1d179d7d062fa49caf996fffc8
Nichol <nicholjohnfamadico@gmail.com>
2021-06-01 13:30:43 +0800

    updates

--- dvdrkweenproject/asgi.py
+++ dvdrkweenproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dvdrkween project.
+ASGI config for dvdrkweenproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dvdrkween.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dvdrkweenproject.settings')
 
 application = get_asgi_application()
