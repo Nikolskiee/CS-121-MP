117439eb6be940678a9b414a52afa5d7e64d2d2e
Nichol <nicholjohnfamadico@gmail.com>
2021-06-02 14:26:55 +0800

    heroku try

--- project/project/settings.py
+++ project/project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3xw-i09=@y&3e5%5f&a+753^5&-tezxp)gmfw)wm0ej=&$f*)_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -80,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
