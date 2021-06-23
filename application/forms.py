962d6eb9cfd34bb66212eb6fe69d41cec76b3cae
WillNigel23 <wcdejesus@up.edu.ph>
2021-06-23 15:16:10 +0800

    Added New Forms
    D ko sure if magerror charfield pero number input

--- application/forms.py
+++ application/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, TextInput, PasswordInput, CharField, HiddenInput, NumberInput
+from django.forms import ModelForm, TextInput, PasswordInput, CharField, HiddenInput, NumberInput, widgets
 from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.models import User
@@ -28,4 +28,34 @@ class CartForm(ModelForm):
             'user' : HiddenInput(attrs = {'type' : 'hidden'}),
             'product' : HiddenInput(attrs = {'type' : 'hidden'}),
             'quantity' : NumberInput(attrs = {'class': 'form-control', 'min' : '1'}),
+        }
+
+class CODForm(ModelForm):
+    class Meta:
+        model = User_COD
+        fields = ['province', 'municipality', 'barangay', 'hs_num']
+
+        widgets = {
+            'province' : HiddenInput(attrs = {'type' : 'hidden'}),
+            'municipality' : HiddenInput(attrs = {'type' : 'hidden'}),
+            'barangay' : HiddenInput(attrs = {'type' : 'hidden'}),
+            'hs_num' : HiddenInput(attrs = {'type' : 'hidden'})
+        }
+
+class CreditForm(ModelForm):
+    class Meta:
+        model = User_Credit
+        fields = ['province', 'municipality', 'barangay', 'hs_num', 'full_name', 'card_number', 'mm', 'dd', 'yyyy', 'ccv']
+
+        widgets = {
+            'province' : HiddenInput(attrs = {'type' : 'hidden'}),
+            'municipality' : HiddenInput(attrs = {'type' : 'hidden'}),
+            'barangay' : HiddenInput(attrs = {'type' : 'hidden'}),
+            'hs_num' : HiddenInput(attrs = {'type' : 'hidden'}),
+            'full_name' : HiddenInput(attrs = {'type' : 'hidden'}),
+            'card_number' : NumberInput(attrs = {'class': 'form-control'}),
+            'mm' : NumberInput(attrs = {'class': 'form-control', 'min' : '1', 'max': '12'}),
+            'dd' : NumberInput(attrs = {'class': 'form-control', 'min' : '1', 'max': '31'}),
+            'yyyy' : NumberInput(attrs = {'class': 'form-control', 'min' : '0', 'max': '2021'}),
+            'ccv' : NumberInput(attrs = {'class': 'form-control', 'min' : '0', 'max' : '999'})
         }
